dc33e6006c014a1e1a5fe62e766fa6e2f020ee80
unknown <vladimirmercado2001@gmail.com>
2021-11-12 08:41:56 -0500

    second commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const app = require('./index')
+
+
+
+function main() {
+    app.listen(6000)
+    console.log("server in port 6000")
+    
+}
+
+main()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
+const morgan = require('morgan')
 
+const app = express()
+
+app.use(morgan('dev'));
+
+
+
+module.exports =app;
